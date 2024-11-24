080f8f3a87ef8b3dc81715329d7d131105bdd8bd
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-11-24 17:23:38 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/tejas8668/Telegram-Master-Auto-Forward-Bot /LazyDeveloper
+cd /LazyDeveloper
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
