01e6792ba80600cb691ab4ad20dbd3e9da73f46a
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-11-25 21:21:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,3 @@
-#    Copyright (c) 2021 Ayush
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
-
 import logging
 from pyrogram import Client, filters
 from decouple import config
